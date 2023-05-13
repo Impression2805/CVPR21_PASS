8b799939e8a86b7b29d17f0745e3d9be28db1fa3
Impression2805 <74761347+Impression2805@users.noreply.github.com>
2023-05-13 16:07:39 +0800

    Update main_PASS_imagenet.py

--- ImageNet-Subset/main_PASS_imagenet.py
+++ ImageNet-Subset/main_PASS_imagenet.py
@@ -228,6 +228,7 @@ def main():
 
     ###################################### test stage ###########################################
     test_transform = transforms.Compose([
+        transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
